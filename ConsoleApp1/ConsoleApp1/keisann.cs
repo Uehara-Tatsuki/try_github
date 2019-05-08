ff97c4297221829eaa9533f85e9d671eaa83d95e
Uehara-Tatsuki <tatsuki_u@occ.co.jp>
2019-05-08 11:37:36 +0900

    計算式

---
+++ ConsoleApp1/ConsoleApp1/keisann.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class keisann
+    {
+        public int App (int a, int b)
+        {
+            int r = a - b;
+            return r;
+
+        }
+
+
+    }
